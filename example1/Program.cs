40fe7def01f805d5a74e0f1e03f56f3c435bd2c9
RegisBarros <regisbarros21@gmail.com>
2019-09-14 20:06:08 -0300

    First log sample

---
+++ example1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Serilog;
+
+namespace LogApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var log = new LoggerConfiguration()
+                .WriteTo.Console()
+                .CreateLogger();
+
+            log.Information("Hello, Serilo!");
+
+            Log.Logger = log;
+
+            MyMethod();
+        }
+
+        static void MyMethod()
+        {
+            Log.Information("The global logger has been configured");
+        }
+    }
+}
