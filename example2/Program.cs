4c10ab1aa356422813bd7ba79f158f468dc6788f
RegisBarros <regisbarros21@gmail.com>
2019-09-14 20:51:01 -0300

    example 2

---
+++ example2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Serilog;
+
+namespace example2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console()
+                .WriteTo.File("logs/logs.txt", rollingInterval: RollingInterval.Day)
+                .CreateLogger();
+
+            Log.Information("Hello World!");
+
+            int a = 10, b = 0;
+
+            try
+            {
+                Log.Debug("Dividing {A} by {B}", a, b);
+                Console.WriteLine(a / b);
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex, "Something went wrong");
+            }
+
+            Log.CloseAndFlush();
+        }
+    }
+}
