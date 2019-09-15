e8b7092a71a11f33de685d82eef064b3be9bd25e
RegisBarros <regisbarros21@gmail.com>
2019-09-15 15:02:12 -0300

    Samples 4

---
+++ example4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using Microsoft.Extensions.Configuration;
+using Serilog;
+
+namespace example4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var logger = new LoggerConfiguration()
+                .ReadFrom.Configuration(configuration)
+                .CreateLogger();
+
+            logger.Information("Hello World!");
+        }
+    }
+}
